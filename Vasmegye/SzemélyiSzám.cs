92d7cc56b4b0d7ced78385bd75a4ae5a96069d4f
zeon77 <zeon77@gmail.com>
2021-06-16 10:50:51 +0200

    3. feladat

--- Vasmegye/SzemélyiSzám.cs
+++ Vasmegye/SzemélyiSzám.cs
@@ -10,6 +10,24 @@ namespace Vasmegye
         //Properties
         public string Azonosító { get; private set; }
 
+        //Methods
+        //3.
+        public bool CdvEll(string sz)
+        {
+            sz = sz.Replace("-", "");
+            int Sum = 0;
+
+            for (int i = 0; i < sz.Length - 1; i++)
+            {
+                Sum += int.Parse(sz[i].ToString()) * (sz.Length - 1 - i);
+            }
+
+            if (Sum % sz.Length == int.Parse(sz[sz.Length - 1].ToString()))
+                return true;
+
+            return false;
+        }
+
         //Constructor
         public SzemélyiSzám(string sor)
         {
